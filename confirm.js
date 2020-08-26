111ea94bdd817e514ab404c2f0acb216a3a827a6
Abuhajer <Abuhagerr@gmail.com>
2020-08-26 12:17:01 +0300

    Add functions

--- confirm.js
+++ confirm.js
@@ -2,15 +2,36 @@
 
 var con=confirm("Do You want us send u massges for the new movies we have !")
 var str;
-if(con==true)
-{
-Email=prompt("Please Give us Your Email");
-str="Thank you for answer , we will make you updating with our movie "
-document.write('<h3>'+str+'</h3>')
-}
-else
-{
-str= "Its oke , We glad u are here <3 ";  
-document.write('<h3>'+str+'</h3>')
 
+function Massage(flag,email)
+{
+    if(flag==true)
+    {
+    Email =email;
+      if(Email==null || Email=="")
+         EmailConf();
+      else
+        {
+          str="Thank you for answer , we will make you updating with our movie ";
+          document.write('<h3>'+str+'</h3>');
+          }
+
+    }
+    else
+    {
+    str= "Its oke , We glad u are here <3 ";  
+    document.write('<h3>'+str+'</h3>');
+    }
 }
+
+function EmailConf()
+{
+ if(con==true)
+ var Email=prompt("Please Give us Your Email");
+
+ Massage(con,Email);
+}
+
+
+
+EmailConf()
--- myscipt.js
+++ myscipt.js
@@ -1,4 +1,18 @@
-var name = prompt ("Enter Your name pls ");
 
+
+function Valid(name)
+{
+if(name==null||name=="")
+Valid(Nameinfo())
+else
 document.write('<h3>'+"Welcome "+name+'</h3>')
 
+}
+
+function Nameinfo()
+{
+    var name = prompt ("Enter Your name pls ");
+    return name
+}
+
+Valid(Nameinfo());
